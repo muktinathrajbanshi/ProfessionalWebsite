be8d158dc6bf4d668296707b31da84881e51963e
unknown <muktinathrajbanshi786@gmail.com>
2025-07-18 23:46:32 +0545

    javascript starts for my portfolio section

---
+++ index.js
@@ -0,0 +1,15 @@
+// creating a portfolio tabbed component
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+
+p_btns.addEventListener("click", (e) => {
+    const p_btn_clicked = e.target;
+    console.log(p_btn_clicked);
+
+    p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+
+    p_btn_clicked.forEach((curElem) => curElem.classList.add("p-btn-active"));
+});
