b4c2ce2db3ddafb6f080430b26454647288a3e45
unknown <muktinathrajbanshi786@gmail.com>
2025-07-20 21:47:16 +0545

    UI is started for testimonial section through css

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ p_btns.addEventListener("click", (e) => {
 // swiper js code
 
  var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 4,
-      spaceBetween: 50,
-      autoplay: {
-        delay:1000,
-        disableOnInteraction: false,
-      },
+      slidesPerView: 2,
+      spaceBetween: 30,
+    //   autoplay: {
+    //     delay:2500,
+    //     disableOnInteraction: false,
+    //   },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
