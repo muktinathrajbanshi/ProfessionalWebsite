9d0c4eb275a7ecc6260b16ed3e54153e98b76a51
unknown <muktinathrajbanshi786@gmail.com>
2025-07-20 22:47:48 +0545

    finally testimonial section completed

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ p_btns.addEventListener("click", (e) => {
  var swiper = new Swiper(".mySwiper", {
       slidesPerView: 2,
       spaceBetween: 30,
-    //   autoplay: {
-    //     delay:2500,
-    //     disableOnInteraction: false,
-    //   },
+      autoplay: {
+        delay:2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
