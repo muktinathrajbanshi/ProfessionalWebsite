6a23ef122fba1f0abf2c29a9a71ec8329b1b8310
unknown <muktinathrajbanshi786@gmail.com>
2025-07-23 14:04:33 +0545

    animated number i.e work counter section animated through js

--- index.js
+++ index.js
@@ -58,3 +58,30 @@ p_btns.addEventListener("click", (e) => {
     };
 
     scrollElement.addEventListener("click", scrollTop);
+
+    // animate number 
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+
+    const speed = 200;
+
+    counterNum.forEach((curElem) => {
+      const updateNumber = () => {
+        const targetNumber = parseInt(curElem.dataset.number);
+        // console.log(targetNumber); 
+        const initialNum = parseInt(curElem.innerText);
+        // console.log(initialNum);
+
+
+        const incrementNumber = Math.trunc(targetNumber/ speed);
+        // console.log(incrementNumber);
+
+        if(initialNum < targetNumber) {
+          curElem.innerText = `${initialNum + incrementNumber}+`;
+
+
+          setTimeout(updateNumber, 10);
+        }
+      };
+      updateNumber();
+    });
