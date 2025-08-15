fb2790a8c0a8d2d1fcee6cc345356368bddfb9ad
unknown <muktinathrajbanshi786@gmail.com>
2025-08-15 20:26:44 +0545

    Added Animation on Number on Scrolling

--- public/index.js
+++ public/index.js
@@ -102,32 +102,51 @@ p_btns.addEventListener("click", (e) => {
 
     scrollElement.addEventListener("click", scrollTop);
 
-    // animate number 
+    
 
-    const counterNum = document.querySelectorAll(".counter-numbers");
+    const workSection = document.querySelector(".section-work-data");
 
-    const speed = 200;
+    const workObserver = new IntersectionObserver((entries, observer) => {
+      const [entry] = entries;
+      console.log(entry);
 
-    counterNum.forEach((curElem) => {
-      const updateNumber = () => {
-        const targetNumber = parseInt(curElem.dataset.number);
-        // console.log(targetNumber); 
-        const initialNum = parseInt(curElem.innerText);
-        // console.log(initialNum);
+      if(!entry.isIntersecting) return; 
+
+      // animate number counter
+
+          const counterNum = document.querySelectorAll(".counter-numbers");
+
+          const speed = 200;
+
+          counterNum.forEach((curElem) => {
+            const updateNumber = () => {
+              const targetNumber = parseInt(curElem.dataset.number);
+              // console.log(targetNumber); 
+              const initialNum = parseInt(curElem.innerText);
+              // console.log(initialNum);
 
 
-        const incrementNumber = Math.trunc(targetNumber/ speed);
-        // console.log(incrementNumber);
+              const incrementNumber = Math.trunc(targetNumber/ speed);
+              // console.log(incrementNumber);
 
-        if(initialNum < targetNumber) {
-          curElem.innerText = `${initialNum + incrementNumber}+`;
+              if(initialNum < targetNumber) {
+                curElem.innerText = `${initialNum + incrementNumber}+`;
 
 
-          setTimeout(updateNumber, 10);
-        }
-      };
-      updateNumber();
+                setTimeout(updateNumber, 10);
+              }
+            };
+            updateNumber();
+          });
+
+          observer.unobserve(workSection);
+
+    }, {
+      root: null,
+      threshold: 0,
     });
+
+    workObserver.observe(workSection);
 
 // lazy loading images 
 
